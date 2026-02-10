5693c38726acb4118e075fe7c9e30d4358f32b7a
apoorva nayak <nayakapoorva2010@gmail.com>
2026-02-10 21:08:53 +0530

    /

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -10,15 +10,16 @@ const {
   orderCompleted,
   cancelOrder,
 } = require("../controllers/order.controller");
+const { requireAuth } = require("../middleware/requireAuth");
 
 const router = express.Router();
 
 // CUSTOMER
 router.post("/", createOrder);
-router.post("/create-pending", createPendingOrder);
-router.post("/create-cod", createCODOrder);
+router.post("/create-pending", requireAuth, createPendingOrder);
+router.post("/create-cod", requireAuth, createCODOrder);
+router.patch("/cancel/:orderId", requireAuth, cancelOrder);
 
-router.patch("/cancel/:orderId", cancelOrder);
 router.get("/order-details/:orderId", fetchOrderDetails);
 router.get("/:userId", fetchUserAllOrders);
 
